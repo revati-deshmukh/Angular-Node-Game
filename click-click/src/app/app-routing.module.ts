6ac2f20fcdc2a414d6fbe064b2a035b11d033b6e
revati-deshmukh <deshmukh.revat@gmail.com>
2023-01-29 22:40:31 +0000

    Added basic app component, routing module and app module

---
+++ click-click/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { RandomClicksComponent } from './random-clicks/random-clicks.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'random-clicks/:level', component: RandomClicksComponent },
+  { path: 'thank-you', component: ThankYouComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ click-click/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RandomClicksComponent } from './random-clicks/random-clicks.component';
+import { NgxTimerModule } from 'ngx-timer';
+import { MainComponent } from './main/main.component';
+import { GameOverComponent } from './game-over/game-over.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
+import { NextLevelComponent } from './next-level/next-level.component';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DialogBoxComponent,
+    RandomClicksComponent,
+    MainComponent,
+    GameOverComponent,
+    ThankYouComponent,
+    NextLevelComponent,
+  ],
+  entryComponents: [
+    DialogBoxComponent,
+    GameOverComponent,
+    NextLevelComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NgxTimerModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
