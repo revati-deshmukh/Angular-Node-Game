57e072ac67c87903c7599d6dbff806f1be6ee3d1
revati-deshmukh <deshmukh.revat@gmail.com>
2023-02-01 12:17:30 +0000

    Made changes in css as well as in html

--- click-click/src/app/random-clicks/random-clicks.component.ts
+++ click-click/src/app/random-clicks/random-clicks.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { GameOverComponent } from '../game-over/game-over.component';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
@@ -6,6 +6,7 @@ import { NextLevelComponent } from '../next-level/next-level.component';
 import { CountupTimerService, countUpTimerConfigModel, timerTexts } from 'ngx-timer';
 import { ElementRef } from '@angular/core';
 declare let $: any;
+
 @Component({
   selector: 'app-random-clicks',
   templateUrl: './random-clicks.component.html',
@@ -19,8 +20,7 @@ export class RandomClicksComponent {
   counter: number;
   someSubscription: any;
   testConfig: countUpTimerConfigModel;
-
-  container = document.querySelector(".container");
+  changedbtnPos: boolean;
 
   colors = ["Blue", "Red", "Green"];
   colorCodes = ["#0000FF", "#FF0000", "#008000"];
@@ -41,12 +41,11 @@ export class RandomClicksComponent {
     this.counter = 0;
     this.route.params.subscribe((params) => {
       this.level = Number(params['level']);
-      console.log(this.level);
     });
     if (this.level > 5) {
-      this.router.navigate(['/thank-you']);
+      this.router.navigate(['/thank-you/completed']);
     }
-    // this.changePosition();
+
     // Calling this function to get first color
     this.generateRandomColor();
 
@@ -70,6 +69,7 @@ export class RandomClicksComponent {
   ngOnDestroy() {
     if (this.someSubscription) {
       this.someSubscription.unsubscribe();
+      this.changedbtnPos = false;
     }
   }
 
@@ -80,7 +80,7 @@ export class RandomClicksComponent {
     this.color = this.colors[index];
     this.colorCode = this.colorCodes[index];
     this.counter += 1;
-    // this.changePosition();
+    this.changePosition();
   }
 
   // Get value of a selected button
@@ -100,7 +100,7 @@ export class RandomClicksComponent {
       if (this.level != 5) {
         this.nextLevelDialog();
       } else {
-        this.router.navigate(['/thank-you']);
+        this.router.navigate(['/thank-you/completed']);
       }
     }
   }
@@ -118,7 +118,7 @@ export class RandomClicksComponent {
       if (result) {
         this.router.navigate(['/']);
       } else {
-        this.router.navigate(['/thank-you']);
+        this.router.navigate(['/thank-you/gameover']);
       }
     });
   }
@@ -136,7 +136,7 @@ export class RandomClicksComponent {
       if (result) {
         this.router.navigate(['/random-clicks', this.level + 1]);
       } else {
-        this.router.navigate(['/thank-you']);
+        this.router.navigate(['/thank-you/completed']);
       }
     });
   }
@@ -147,27 +147,39 @@ export class RandomClicksComponent {
   }
 
   // Adding event listener
-  /* changePosition(): void {
+  changePosition(): void {
+    this.changedbtnPos = true;
+    const containerEle = document.getElementById('container');
+    var cWidth, cHeight, bWidth, bHeight;
+    if (containerEle) {
+      cWidth = containerEle.getBoundingClientRect().width; // Get width of the container
+      cHeight = containerEle.getBoundingClientRect().height; // Get height of the container
+      console.log(cWidth + " " + cHeight);
+    }
 
-    const
-      { width: cWidth, height: cHeight } = $(".container").getBoundingClientRect(),
-      { width: bWidth, height: bHeight } = $("#red").getBoundingClientRect(),
-      i = Math.floor(Math.random() * (cWidth - bWidth)) + 1,
-      j = Math.floor(Math.random() * (cHeight - bHeight)) + 1;
+    const buttonEle = document.getElementById('red');
+    if (buttonEle) {
+      bWidth = buttonEle.getBoundingClientRect().width; // Get width of the button
+      bHeight = buttonEle.getBoundingClientRect().height; // Get height of the button
+      console.log(bWidth + " " + bHeight);
+    }
 
-    //var redTop = Math.floor(Math.random() * 400) + 1;
-    //var redLeft = Math.floor(Math.random() * 400) + 1;
-    $("#red").css("margin-top", i + "px");
-    $("#red").css("margin-left", j + "px");
+    var redWidth = Math.floor(Math.random() * (cWidth - 2 * bWidth)) + 1;
+    var redHeight = Math.floor(Math.random() * (cHeight - 2 * bHeight)) + 1;
+    $("#red").css("top", redHeight + "px");
+    $("#red").css("left", redWidth + "px");
+    $("#red").css("margin", "3px");
 
-    var blueTop = Math.floor(Math.random() * 500) + 1;
-    var blueLeft = Math.floor(Math.random() * 500) + 1;
-    $("#blue").css("margin-top", blueTop + "px");
-    $("#blue").css("margin-left", blueLeft + "px");
+    var blueWidth = Math.floor(Math.random() * (cWidth - 2 * bWidth)) + 1;
+    var blueHeight = Math.floor(Math.random() * (cHeight - 2 * bHeight)) + 1;
+    $("#blue").css("top", blueHeight + "px");
+    $("#blue").css("left", blueWidth + "px");
+    $("#blue").css("margin", "3px");
 
-    var greenTop = Math.floor(Math.random() * 500) + 1;
-    var greenLeft = Math.floor(Math.random() * 500) + 1;
-    $("#green").css("margin-top", greenTop + "px");
-    $("#green").css("margin-left", greenLeft + "px");
-  } */
+    var greenWidth = Math.floor(Math.random() * (cWidth - 2 * bWidth)) + 1;
+    var greenHeight = Math.floor(Math.random() * (cHeight - 2 * bHeight)) + 1;
+    $("#green").css("top", greenHeight + "px");
+    $("#green").css("left", greenWidth + "px");
+    $("#green").css("margin", "3px");
+  }
 }
